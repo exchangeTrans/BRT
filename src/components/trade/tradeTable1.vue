5445e5cbc341dd6a5b89641078181faf7952a608
zhangtao5298 <18280092045@163.com>
2020-09-12 22:22:27 +0800

    change

---
+++ src/components/trade/tradeTable1.vue
@@ -0,0 +1,176 @@
+<template>
+	<view class="tradeTable1" id="tradeTable1">
+		<view class="tableHeader">
+            <view class="tableLi tableLi1">买盘</view>
+            <view class="tableLi tableLi2">数量(LED)</view>
+            <view class="tableLi tableLi3">价格(HDU)</view>
+            <view class="tableLi tableLi4">数量(LED)</view>
+            <view class="tableLi tableLi5">卖盘</view>
+        </view>
+        <view class="tableTr">
+            <view class="tableLi tableLi1">1</view>
+            <view class="tableLi tableLi2">1200.96</view>
+            <view class="tableLi tableLi3">
+                <view class="price1">0.0639</view>
+                <view class="price2">0.0639</view>
+            </view>
+            <view class="tableLi tableLi4">1200.96</view>
+            <view class="tableLi tableLi5">1</view>
+        </view>
+        <view class="tableTr">
+            <view class="tableLi tableLi1">1</view>
+            <view class="tableLi tableLi2">1200.96</view>
+            <view class="tableLi tableLi3">
+                <view class="price1">0.0639</view>
+                <view class="price2">0.0639</view>
+            </view>
+            <view class="tableLi tableLi4">1200.96</view>
+            <view class="tableLi tableLi5">1</view>
+        </view>
+        <view class="tableTr">
+            <view class="tableLi tableLi1">1</view>
+            <view class="tableLi tableLi2">1200.96</view>
+            <view class="tableLi tableLi3">
+                <view class="price1">0.0639</view>
+                <view class="price2">0.0639</view>
+            </view>
+            <view class="tableLi tableLi4">1200.96</view>
+            <view class="tableLi tableLi5">1</view>
+        </view>
+	</view>
+</template>
+
+<script>
+    export default {
+	    components:{
+		},
+        data() {
+            return {
+                headerOptions: {
+                    show: true,
+                    isAllowReturn: true,
+                    text: "LED/HDU",
+                    rightItem: {
+                        // type: "text",
+                        // text: "须知&反馈",
+                        // style: {
+                        //     "fontSize": '28rpx',
+                        //     "fontFamily": 'Source Han Sans CN',
+                        //     "fontWeight": '400',
+                        //     "color": 'rgba(68,68,68,1)'
+                        // }
+                    },
+                    headerIsNoBoder: false,
+                },
+                chartTabArray:[
+                    {code:'1',name:"1分",id:''},
+                    {code:'2',name:"15分",id:''},
+                    {code:'3',name:"1小时",id:''},
+                    {code:'4',name:"4小时",id:''},
+                    {code:'5',name:"日线",id:''},
+                    {code:'6',name:"周线",id:''},
+                    {code:'7',name:"指标",id:''},
+                ],
+                chartTabSelect:{code:'2',name:"15分",id:''},
+                tableTabArray:[
+                    {code:'1',name:"买单/卖单",id:''},
+                    {code:'2',name:"最新成交",id:''},
+                    {code:'3',name:"币种简介",id:''},
+                ],
+                tableTabSelect:{code:'2',name:"最新成交",id:''},
+                
+
+            }
+        },
+        onLoad() {
+
+        },
+        methods: {
+            selectChartTab(item){
+                this.chartTabSelect = item;
+            },
+            selectTableTab(item){
+                this.tableTabSelect = item;
+            },
+        }
+    }
+</script>
+
+<style lang="less">
+	.tradeTable1{
+		width: 100vw;
+        height: auto;
+        .tableHeader{
+            width: 100%;
+            height: 74rpx;
+            box-sizing: border-box;
+            padding: 0 20rpx;
+            display: flex;
+            justify-content:space-between;
+            .tableLi{      
+                height: 74rpx;      
+                font-size: 24rpx;
+                font-family: PingFangSC-Regular, PingFang SC;
+                font-weight: 400;
+                color: #A5AEB7;
+                line-height: 74rpx;
+                text-align: center;
+            }
+            .tableLi1,.tableLi5{
+                width: 48rpx;
+            }
+            .tableLi2{
+                width: 130rpx;
+            }
+            .tableLi3{
+                width: 230rpx;
+            }
+            .tableLi4{
+                width: 130rpx;
+            }
+        }
+        .tableTr{
+            width: 100%;
+            height: 88rpx;
+            box-sizing: border-box;
+            padding: 0 20rpx;
+            display: flex;
+            justify-content:space-between;
+            .tableLi{      
+                height: 88rpx;      
+                font-size: 24rpx;
+                font-family: PingFangSC-Regular, PingFang SC;
+                font-weight: 400;
+                color: #1A1A1A;
+                line-height: 88rpx;
+                text-align: center;
+            }
+            .tableLi1,.tableLi5{
+                width: 48rpx;
+                opacity: 0.5;
+            }
+            .tableLi2{
+                width: 130rpx;
+            }
+            .tableLi3{
+
+                width: 230rpx;
+                display: flex;
+                justify-content: center;
+                .price1{
+                    margin-right: 10rpx;
+                    color: #FC3C5A;
+                }
+                .price2{
+                    color: #5BC788;
+                }
+            }
+            .tableLi4{
+                width: 130rpx;
+            }
+        }
+		
+
+	}
+
+</style>
