64e7db85eb5a4b78a993b51c69341494c43dd78c
coderybh <2046230119@qq.com>
2020-09-27 01:06:41 +0800

    ybh

---
+++ src/script/connectus/connectus.js
@@ -0,0 +1,73 @@
+import appheader from '../../components/common/header.vue'
+export default{
+    components:{appheader},
+    data(){
+        return{
+            headerOptions:{
+                // show: true,
+                // isAllowReturn: true,
+                // text: "联系客服",
+                // rightItem: {
+                //     type: "text",
+                //     text: "",
+                // },
+                // bodyPadding: {"padding": '0,0,0,0'},
+                // headerIsNoBoder: true,
+            }
+        }
+    },
+    mounted() {
+        let theme = this.$storage.getSync({
+            key: 'theme'
+        });
+
+        if (theme === 'white') {
+            this.headerOptions = {
+                show: true,
+                isAllowReturn: true,
+                text: "联系客服",
+                background: "#FFFFFF",
+                rightItem: {
+                    type: "text",
+                    text: "ID:AVV941",
+                    style: {
+                        "fontSize": '24rpx',
+                        "fontFamily": 'PingFangSC-Regular, PingFang SC;',
+                        "fontWeight": '400',
+                        "color": 'rgba(68,68,68,1)'
+                    }
+                },
+                style: {
+                    color: "#000000"
+                },
+                isWhiteIcon: false,
+                headerIsNoBoder: false,
+            }
+        } else {
+            this.headerOptions={
+                show: true,
+                isAllowReturn: true,
+                text: "联系客服",
+                background: '#00001A',
+                isWhiteIcon: true,
+                rightItem: {
+                    type: "text",
+                    text: "ID:AVV941",
+                    style: {
+                        "fontSize": '24rpx',
+                        "fontFamily": 'PingFangSC-Regular, PingFang SC;',
+                        "fontWeight": '400',
+                        "color": 'rgba(68,68,68,1)',
+                        style: {
+                            color: "#D9DADB"
+                        }
+                    }
+                },
+                style: {
+                    color: "#D9DADB"
+                },
+                headerIsNoBoder: true,
+            }
+        }
+    }
+}
